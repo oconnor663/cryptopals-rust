25c946ff564c24f3963faf736da88f840b053de8
Jack O'Connor <oconnor663@gmail.com>
2016-01-02 18:14:45 -0500

    implement xor

--- set1.rs
+++ set1.rs
@@ -1,6 +1,18 @@
+const HEX_ALPHABET: &'static [u8] = b"0123456789abcdef";
 const BASE64_ALPHABET: &'static [u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
-fn base64(bytes: &[u8]) -> String {
+fn from_hex(input: &[u8]) -> Vec<u8> {
+    assert!(input.len() % 2 == 0, "hex strings must be even length");
+    let mut ret = Vec::new();
+    for i in 0 .. input.len()/2 {
+        let first_index = HEX_ALPHABET.iter().position(|b| *b == input[2*i]).unwrap() as u8;
+        let second_index = HEX_ALPHABET.iter().position(|b| *b == input[2*i+1]).unwrap() as u8;
+        ret.push(16 * first_index + second_index);
+    }
+    ret
+}
+
+fn to_base64(bytes: &[u8]) -> String {
     let mut accumulator = 0usize;
     let mut accumulated_bits = 0;
     let mut ret = "".to_string();
@@ -27,9 +39,30 @@ fn base64(bytes: &[u8]) -> String {
     ret
 }
 
+fn xor(left: &[u8], right: &[u8]) -> Vec<u8> {
+    let mut ret = Vec::new();
+    for i in 0..std::cmp::max(left.len(), right.len()) {
+        let mut val = 0;
+        if i < left.len() {
+            val ^= left[i];
+        }
+        if i < right.len() {
+            val ^= right[i];
+        }
+        ret.push(val);
+    }
+    ret
+}
+
 fn main() {
-    let b = b"foobarba";
-    println!("{:?}", String::from_utf8(b.to_vec()));
-    let s = base64(b);
-    println!("{}", s);
+    let input = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    println!("input {}", std::str::from_utf8(input).unwrap());
+    let bytes = from_hex(input);
+    let output = to_base64(&*bytes);
+    println!("{}", output);
+
+    let xor_left = [1, 1, 1];
+    let xor_right = [2, 2, 2, 2];
+    let xor_result = xor(&xor_left, &xor_right);
+    println!("xor result: {:?}", xor_result);
 }

f654d2df0c0a3b33ed0dbc6fe3a5bfd24cc165df
Jack O'Connor <oconnor663@gmail.com>
2020-02-16 15:50:32 -0500

    3.21

--- set3/src/main.rs
+++ set3/src/main.rs
@@ -241,6 +241,71 @@ fn score(bytes: &[u8]) -> f32 {
     normalized_dot_product(&w, &f)
 }
 
+const F: u32 = 1812433253;
+const W: u32 = 32;
+const N: usize = 624;
+const M: usize = 397;
+const R: u32 = 31;
+const A: u32 = 0x9908B0DF;
+const U: u32 = 11;
+const D: u32 = 0xFFFFFFFF;
+const S: u32 = 7;
+const B: u32 = 0x9D2C5680;
+const T: u32 = 15;
+const C: u32 = 0xEFC60000;
+const L: u32 = 18;
+const LOWER_MASK: u32 = (1 << R) - 1; // That is, the binary number of r 1's
+const UPPER_MASK: u32 = !LOWER_MASK; // lowest w bits of (not lower_mask)
+
+struct MT {
+    array: [u32; N],
+    index: usize,
+}
+
+impl MT {
+    fn seed(seed: u32) -> Self {
+        let mut array = [0; N];
+        array[0] = seed;
+        for i in 1..N {
+            array[i] = F
+                .wrapping_mul(array[i - 1] ^ (array[i - 1] >> (W - 2)))
+                .wrapping_add(i as u32);
+        }
+        Self { array, index: N }
+    }
+
+    fn extract_number(&mut self) -> u32 {
+        if self.index >= N {
+            if self.index > N {
+                panic!();
+            }
+            self.twist();
+        }
+
+        let mut y = self.array[self.index];
+        y ^= (y >> U) & D;
+        y ^= (y << S) & B;
+        y ^= (y << T) & C;
+        y ^= y >> L;
+
+        self.index += 1;
+        y
+    }
+
+    fn twist(&mut self) {
+        for i in 0..N - 1 {
+            let x = (self.array[i] & UPPER_MASK) + (self.array[(i + 1) % N] & LOWER_MASK);
+            let mut x_a = x >> 1;
+            if (x % 2) != 0 {
+                // lowest bit of x is 1
+                x_a = x_a ^ A;
+            }
+            self.array[i] = self.array[(i + M) % N] ^ x_a;
+        }
+        self.index = 0;
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Challenge 17
     println!("============ challenge 17 =============");
@@ -296,6 +361,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         xor(&mut buf, mask);
         println!("{:?}", String::from_utf8_lossy(&buf));
     }
+
+    // Challenge 20 is basically the above.
+
+    // Challenge 21
+    let mut rng = MT::seed(0);
+    assert_eq!(2357136044, rng.extract_number());
+    assert_eq!(2546248239, rng.extract_number());
+    assert_eq!(3071714933, rng.extract_number());
+    assert_eq!(3626093760, rng.extract_number());
 
     Ok(())
 }

672f83e86ffbf9c045754b8dfa667cfac76b7e7e
Jack O'Connor <oconnor663@gmail.com>
2020-02-17 15:38:52 -0500

    4.27

--- set4/src/main.rs
+++ set4/src/main.rs
@@ -3,11 +3,44 @@ use block_cipher_trait::BlockCipher;
 use once_cell::sync::Lazy;
 use rand::{thread_rng, Rng};
 
+fn pad(input: &[u8], block_len: usize) -> Vec<u8> {
+    let mut out = input.to_vec();
+    let padding_bytes = block_len - (input.len() % block_len);
+    for _ in 0..padding_bytes {
+        out.push(padding_bytes as u8);
+    }
+    out
+}
+
+fn unpad(input: &[u8]) -> Result<Vec<u8>, ()> {
+    let last = *input.last().unwrap();
+    if last == 0 {
+        return Err(());
+    }
+    if last as usize > input.len() {
+        return Err(());
+    }
+    for i in input.len() - last as usize..input.len() {
+        if input[i] != last {
+            return Err(());
+        }
+    }
+    Ok(input[0..input.len() - last as usize].to_vec())
+}
+
 fn aes128_encrypt_block(key: &[u8; 16], block: &mut [u8]) {
     assert_eq!(block.len(), 16);
     let mut block_array = (*array_ref!(block, 0, 16)).into();
     let cipher = aesni::Aes128::new(&((*key).into()));
     cipher.encrypt_block(&mut block_array);
+    block.copy_from_slice(&block_array);
+}
+
+fn aes128_decrypt_block(key: &[u8; 16], block: &mut [u8]) {
+    assert_eq!(block.len(), 16);
+    let mut block_array = (*array_ref!(block, 0, 16)).into();
+    let cipher = aesni::Aes128::new(&((*key).into()));
+    cipher.decrypt_block(&mut block_array);
     block.copy_from_slice(&block_array);
 }
 
@@ -70,7 +103,7 @@ fn edit(ciphertext: &mut [u8], key: &[u8; 16], offset: usize, newtext: &[u8]) {
     ctr_xor_seek(key, slice, offset);
 }
 
-fn encrypt_userdata(data: &[u8]) -> Vec<u8> {
+fn encrypt_userdata_ctr(data: &[u8]) -> Vec<u8> {
     assert!(!data.contains(&(';' as u8)));
     assert!(!data.contains(&('=' as u8)));
     let mut content = b"comment1=cooking%20MCs;userdata=".to_vec();
@@ -81,13 +114,66 @@ fn encrypt_userdata(data: &[u8]) -> Vec<u8> {
     content
 }
 
-fn user_is_admin(ciphertext: &[u8]) -> bool {
+fn user_is_admin_ctr(ciphertext: &[u8]) -> bool {
     let key = b"secret key!!!!!!";
     let mut plaintext = ciphertext.to_vec();
     ctr_xor(key, &mut plaintext);
     String::from_utf8_lossy(&plaintext)
         .find(";admin=true;")
         .is_some()
+}
+
+fn cbc_encrypt(key: &[u8; 16], iv: &[u8], input: &[u8]) -> Vec<u8> {
+    let mut out = pad(input, 16);
+    assert_eq!(out.len() % 16, 0);
+    let mut last = iv;
+    for block in out.chunks_exact_mut(16) {
+        xor(block, last);
+        aes128_encrypt_block(key, block);
+        last = block;
+    }
+    out
+}
+
+fn cbc_decrypt(key: &[u8; 16], iv: &[u8], ciphertext: &[u8]) -> Vec<u8> {
+    assert_eq!(ciphertext.len() % 16, 0);
+    let mut plaintext = ciphertext.to_vec();
+    let mut last = *array_ref!(iv, 0, 16);
+    for block in plaintext.chunks_exact_mut(16) {
+        let ciphertext_copy = *array_ref!(block, 0, 16);
+        aes128_decrypt_block(key, block);
+        xor(block, &last);
+        last = ciphertext_copy;
+    }
+    unpad(&plaintext).unwrap()
+}
+
+fn encrypt_userdata_cbc_key_as_iv(data: &[u8]) -> Vec<u8> {
+    assert!(!data.contains(&(';' as u8)));
+    assert!(!data.contains(&('=' as u8)));
+    let mut content = b"comment1=cooking%20MCs;userdata=".to_vec();
+    content.extend_from_slice(data);
+    content.extend_from_slice(b";comment2=%20like%20a%20pound%20of%20bacon");
+    let key = b"secret key!!!!!!";
+    cbc_encrypt(key, key, &content)
+}
+
+#[derive(Debug)]
+struct NonAsciiError {
+    plaintext: Vec<u8>,
+}
+
+fn user_is_admin_cbc_key_as_iv(ciphertext: &[u8]) -> Result<bool, NonAsciiError> {
+    let key = b"secret key!!!!!!";
+    let plaintext = cbc_decrypt(key, key, ciphertext);
+    for &b in &plaintext {
+        if b >= 128 {
+            return Err(NonAsciiError { plaintext });
+        }
+    }
+    Ok(String::from_utf8_lossy(&plaintext)
+        .find(";admin=true;")
+        .is_some())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -107,14 +193,34 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     assert_eq!(&secret_plaintext[..], &ciphertext[..]);
 
     // Challenge 26
-    let mut ciphertext = encrypt_userdata(&[]);
+    let mut ciphertext = encrypt_userdata_ctr(&[]);
     let known_plaintext =
         b"comment1=cooking%20MCs;userdata=;comment2=%20like%20a%20pound%20of%20bacon";
     let target_plaintext =
         b";admin=true;                                                              ";
     xor(&mut ciphertext, known_plaintext);
     xor(&mut ciphertext, target_plaintext);
-    assert!(user_is_admin(&ciphertext));
+    assert!(user_is_admin_ctr(&ciphertext));
+
+    // Challenge 27
+    eprintln!("============ challenge 27 ===================");
+    let example = encrypt_userdata_cbc_key_as_iv(&[0; 16 * 3]);
+    assert!(!user_is_admin_cbc_key_as_iv(&example).unwrap());
+    // We need a block that decrypts to all 0's (prior to the xor step). To get
+    // that, put a plaintext block in the message that's exactly equal to the
+    // ciphertext block before it. (Note that the userdata begins at offset 32,
+    // a block boundary.)
+    let from_chosen_plaintext = encrypt_userdata_cbc_key_as_iv(&example[16..32]);
+    // Now put that block at the front of a message and decrypt it. The 0's
+    // will be xor'd with the key, which should then be reported back to us,
+    // because the garbling is almost guaranteed to produce bad ASCII.
+    let mut chosen_ciphertext = example.clone();
+    chosen_ciphertext[0..16].copy_from_slice(&from_chosen_plaintext[32..48]);
+    let error = user_is_admin_cbc_key_as_iv(&chosen_ciphertext).unwrap_err();
+    eprintln!(
+        "the key is: {:?}",
+        String::from_utf8_lossy(&error.plaintext[..16])
+    );
 
     Ok(())
 }
